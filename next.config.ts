85ae5bcc8b621af1142b4428373e96a0d4ddb9f0
Николай Сергеевич Егоров <nikolay.egorov@russianpost.ru>
2025-06-22 16:35:34 +0300

    fix: add assetPrefix to fix styles on GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/FrontendAssessmentUI",
+  assetPrefix: "/FrontendAssessmentUI/",
   output: "export",
   images: {
     unoptimized: true,
