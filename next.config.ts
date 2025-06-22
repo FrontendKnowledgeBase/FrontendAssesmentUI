4dbc59cf643709b1bcb97f43605c218400028140
Николай Сергеевич Егоров <nikolay.egorov@russianpost.ru>
2025-06-22 17:40:47 +0300

    fix: correct typo in basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/FrontendAssessmentUI",
-  assetPrefix: "/FrontendAssessmentUI/",
+  basePath: "/FrontendAssesmentUI",
   output: "export",
   images: {
     unoptimized: true,
